79a7cd28c9ab5a08795a63738454a27a20bfec6a
WamGam <n11247614@qut.edu.au>
2023-04-17 17:20:18 +1000

    all the changes i made in terms of mechanics

---
+++ IGB100 Project/Assets/StarterAssets/FirstPersonController/Scripts/FindObject.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class FindObject : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] objs;
+    public GameObject[] preObjs;
+    private AudioSource audioDuck;
+    private AudioSource audioDuck2;
+    System.Random rnd = new System.Random();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        preObjs = GameObject.FindGameObjectsWithTag("PreFind");
+        int selection = rnd.Next(0, preObjs.Length - 1);
+        for (int i = 0; i < preObjs.Length; i++)
+        {
+            if (i == selection)
+            {
+                preObjs[i].gameObject.tag = "Finder";
+                break;
+            }
+        }
+        objs = GameObject.FindGameObjectsWithTag("Finder");
+         for (int i = 0; i< objs.Length; i++)
+        {
+            if (i == 0)
+            {
+                audioDuck = objs[i].GetComponent<AudioSource>();
+            }
+            if (i == 1)
+            {
+                audioDuck2 = objs[i].GetComponent<AudioSource>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        findDuck();
+    }
+
+    public void findDuck()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            foreach (var obj in objs)
+            {
+                if (Vector3.Distance(player.transform.position, obj.transform.position) < 50)
+                {
+                    UnityEngine.Debug.Log(obj.transform.position);
+                    audioDuck.Play();
+                    audioDuck2.Play();
+                }
+            }
+        }
+    }
+}
